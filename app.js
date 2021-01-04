d7645a99e7291280a0b575aa503dd5f80c8d8f0c
Nhut Vo <nhutmvo1997@gmail.com>
2021-01-04 16:29:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,31 @@ var bodyparser = require('body-parser');
 const e = require('express');
 var app = express();
 
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "123456789",
+//     database: "nodemysql"
+// });
+
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "nodemysql"
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b83002a7a7af00",
+    password: "7eb049f7",
+    database: "heroku_aea4dd43fd7100b"
 });
 
 con.connect((err) => {
     if (err) {
-        // console.log('Error');
         throw err;
     } else {
         console.log("Database Connected!");
     }
 })
 
-// Create DB
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE '
-})
 
+// Parses the request body and transforms it 
+// into a JavaScript Object for easy operation
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.get('', (req, res) => {
@@ -36,8 +40,21 @@ app.post('/', (req, res) => {
     var n2 = Number(req.body.num2)
     var sum = n1 + n2
     res.send('The value is: ' + sum)
+
+    let sql = "INSERT INTO total VALUES (null, '" + n1 + "', '" + n2 + "'," + sum + ")";
+
+    let query =  con.query(sql, (err, result) => {
+        if(err) throw err;
+        // res.send('Sum added...');
+    });
+
 });
 
-app.listen(3000,(res) => {
-    console.log("server started at port 3000")
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT,(res) => {
+    console.log(`Server started at port ${PORT}. `)
 });
+
+
+
